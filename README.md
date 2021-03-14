98072afd11985657698171772c004a90ac0cb3bc
utsabk <kharelutsab@gmail.com>
2021-03-14 12:57:25 +0200

    Session management

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Cookies
+# Session
 
-ExpressJS - Cookies
+ExpressJS - Session
 
--- app.js
+++ app.js
@@ -1,30 +1,52 @@
 'use strict';
 
 import express from 'express';
+import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
+import session from 'express-session';
 
 const app = express();
 const port = 3000;
+const username = 'foo';
+const password = 'bar';
 
 app.set('views', './views');
 app.set('view engine', 'pug');
 
 app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "Shh, it's a secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
 app.get('/', (req, res) => {
   res.render('home');
 });
 
-app.get('/setCookie/:clr', (req, res) => {
-  const clr = req.params.clr
-  console.log(clr)
-  res.cookie('color', clr).send('cookie set');
-  console.log('Cookies: ', req.cookies);
-})
+app.get('/form', (req, res) => {
+  res.render('form');
+});
 
-app.get('/deleteCookie', (req, res) => {
-  res.clearCookie('color');
-  res.send('cookie cleared');
-})
+app.post('/login', (req, res) => {
+  console.log(req.body);
+  if (req.body.username == username && req.body.password == password) {
+    req.session.logged = true;
+    res.redirect('/secret');
+  } else {
+    req.session.logged = false;
+    res.redirect('/form');
+  }
 
+  console.log('req.session.logged', req.session.logged);
+});
+
+app.get('/secret', (req, res) => {
+  console.log('req.session.logged', req.session.logged);
+  !req.session.logged ? res.redirect('/form') : res.render('secret');
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
